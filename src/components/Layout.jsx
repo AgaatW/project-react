287d1b832e0e0980a9b7abd832597cb2ecde75af
Agata Wnek <knopagata@gmail.com>
2025-02-01 12:45:05 +0100

    add layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import { Fragment } from "react";
+import { Link, NavLink} from "react-router-dom";
+
+const navLinks = [
+  { title: "Home", to: "/", icon: "feather-home" },
+  { title: "Stories", to: "/defaultstorie", icon: "feather-zap" },
+  { title: "Videos", to: "/defaultvideo", icon: "feather-video" },
+  { title: "Groups", to: "/defaultgroup", icon: "feather-user" },
+  { title: "Shop", to: "/shop2", icon: "feather-shopping-bag" },
+];
+
 
 export const Layout = ({ children }) => (
+  <Fragment>
   <div className="nav-header bg-white shadow-xs border-0">
     <div className="nav-top">
     <Link to="/">
@@ -10,6 +21,45 @@ export const Layout = ({ children }) => (
         </span>
       </Link>
     </div>
+
+    <form action="#" className="float-left header-search ms-3">
+      <div className="form-group mb-0 icon-input">
+        <i className="feather-search font-sm text-grey-400"></i>
+        <input
+          type="text"
+          placeholder="Start typing to search.."
+          className="bg-grey border-0 lh-32 pt-2 pb-2 ps-5 pe-3 font-xssss fw-500 rounded-xl w350 theme-dark-bg"
+        />
+      </div>
+    </form>
+
+    {navLinks.map((navLink) => (
+      <NavLink
+      key={navLink.title}
+        activeClassName="active"
+        to={navLink.to}
+        className="p-2 text-center ms-0 menu-icon center-menu-icon"
+      >
+        <i
+          className={`${navLink.icon} font-lg bg-greylight btn-round-lg theme-dark-bg text-grey-500`}
+        ></i>
+      </NavLink>
+    ))}
+
+{/* desktop menu */}
+
+<Link
+      to="/defaultmessage"
+      className="p-2 text-center ms-3 menu-icon chat-active-btn"
+    >
+      <i className="feather-message-square font-xl text-current"></i>
+    </Link>
+
+    <Link to="defaultsettings" className="p-0 ms-3 menu-icon">
+      <img src="assets/images/user.png" alt="user" className="w40 mt--1" />
+    </Link>
+
     {children}
   </div>
-);
+  </Fragment>
+  );
