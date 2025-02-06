80e9a47ecdb8462fb5702bb995dd8000153e5f52
AgaatW <knopagata@gmail.com>
2025-02-06 21:02:05 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,47 @@
-import { Layout } from "../components/Layout";
 
-export const Home = () => (
-  <Layout>
-    <div className="main-content right-chat-active">
-      <div className="middle-sidebar-bottom">
-        <div className="middle-sidebar-left">
-          <div className="row feed-body"></div>
+import { useEffect, useState } from "react";
+import { Layout } from "../components/Layout/Layout";
+import { Postview } from "../components/Postview";
+import Loader from "../components/Loader";
+
+
+export const Home = () => {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    fetch("https://my.api.mockaroo.com/posts.json?key=6d1737e0")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  console.log("posts from Home component:", posts);
+
+  const updatedPosts = posts?.map((post) => {
+    return {
+      ...post,
+      postimage: `https://picsum.photos/id/${post.id}/800/400`,
+    };
+  });
+
+  return (
+    <Layout>
+      <div className="main-content right-chat-active">
+        <div className="middle-sidebar-bottom">
+          <div className="middle-sidebar-left">
+            <div className="row feed-body">
+              <div className="col-xl-8 col-xxl-9 col-lg-8">
+              {!updatedPosts || updatedPosts.length < 1 ? (
+                  <Loader />
+                ) : (
+                  updatedPosts.map((post) => (
+                    <Postview key={post.id} {...post} />
+                  ))
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
